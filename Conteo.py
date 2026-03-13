75131699abe0138b6c42ab1f6771c204bf0fb81e
Soy Francis <francis.arjas@gmail.com>
2026-03-13 15:57:59 -0400

    Update Conteo.py

--- Conteo.py
+++ Conteo.py
@@ -1,12 +1,6 @@
-texto = "gato perro gato pajaro pez gato perro"
-palabras = texto.split()
 contador = {}
 for palabra in palabras:
     if palabra in contador:
         contador[palabra] += 1
     else:
         contador[palabra] = 1
-print("Texto:", texto)
-print("\nConteo de palabras:")
-for palabra, cantidad in contador.items():
-    print(f"{palabra}: {cantidad}:")
