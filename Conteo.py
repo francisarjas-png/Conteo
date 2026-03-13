4124c62858fa6c0f39a42e60094b8c42ddf1c743
francisarjas-png <francis.arjas@gmail.com>
2026-03-13 16:04:55 -0400

    Add files via upload

--- Conteo.py
+++ Conteo.py
@@ -1,6 +1,13 @@
+texto = "Gato Perro gato Pajaro pez GATO perro Pez Gato PAJARO Pez Gato PEZ"
+palabras = texto.lower().split()
 contador = {}
 for palabra in palabras:
     if palabra in contador:
         contador[palabra] += 1
     else:
         contador[palabra] = 1
+palabras_ordenadas = sorted(contador.items(), key=lambda x: x[1], reverse=True)
+print("Texto original:", texto)
+print("\nConteo de palabras")
+for palabra, cantidad in palabras_ordenadas:
+    print(f"{palabra}: {cantidad}")
